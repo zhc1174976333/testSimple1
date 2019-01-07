014db820cd0655d684a902312b9715da6d6aa7ac
haicheng.zhang <&yg6T$8x>
2019-01-07 16:50:06 +0800

    初始化构建2019010701

---
+++ src/main/java/com/zhc/test/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.zhc.test.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "YOU CAN YOU UP,NO CAN NO BB!";
+    }
+}
